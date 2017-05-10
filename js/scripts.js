ceca9089853bb76bcf40d0540b763bfe0cbf19ce
Александр <klesh2@mail.ru>
2017-05-10 19:15:30 +0300

    Финал

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,23 @@ var map_link = document.querySelector(".modal-map");
 var map_popup = document.querySelector(".modal-content-map");
 var map_close = document.querySelector(".modal-content-map .modal-content-close");
 
-feedback_link.addEventListener("click", function(evt) {
+feedback_link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	feedback_popup.classList.add("modal-content-show");
 	feedback_form_name.focus();
 });
 
-feedback_close.addEventListener("click", function(evt) {
+feedback_close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	feedback_popup.classList.remove("modal-content-show");
 })
 
-map_link.addEventListener("click", function(evt) {
+map_link.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	map_popup.classList.add("modal-content-show");
 });
 
-map_close.addEventListener("click", function(evt) {
+map_close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	map_popup.classList.remove("modal-content-show");
 })
